746169d410857d88275f7437f7f8edea642ba2a9
Thulio Philipe <thulioph@gmail.com>
2015-04-28 18:01:26 -0300

    Adds some HTML code to navbar.

---
+++ php/includes/navbar.inc.php
@@ -0,0 +1,28 @@
+<nav id="nav-primary" class="nav-primary">
+  <h1 class="logo-primary">Flu Near You</h1>
+
+  <ul>
+    <li class="nav-item">
+      <a href="#" title="Flu Map" class="nav-link">Flu Map</a>
+      <a href="about" title="About" class="nav-link">About</a>
+      <a href="flu-news" title="News" class="nav-link">News</a>
+      <a href="press" title="Press" class="nav-link">Press</a>
+      <a href="faq" title="FAQ" class="nav-link">FAQ</a>
+    </li>
+  </ul>
+
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+      Dropdown
+      <span class="caret"></span>
+    </button>
+
+    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="settings">Settings</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="reports">Health Reports</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="home">Log out</a></li>
+    </ul>
+  </div>
+
+  <a href="survey" title="Survey" class="btn btn-submit">Survey</a>
+</nav>
