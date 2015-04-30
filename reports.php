292c08bf96ae76f756400b8e514847e0d24cfe77
Thulio Philipe <thulioph@gmail.com>
2015-04-30 03:31:42 -0300

    Adds HTML code to Reports page.

--- reports.php
+++ reports.php
@@ -1,5 +1,40 @@
 <?php include 'php/includes/head.inc.php'; ?>
 <?php include 'php/includes/navbar.inc.php' ?>
 
+<section class="wrapper-section">
+  <aside class="report-card">
+    <h3 class="title-primary">Report Card</h3>
+
+    <ul>
+      <li>
+        <p>Weeks eligible to report</p>
+        <p>20</p>
+      </li>
+
+      <li>
+        <p>Weeks reported</p>
+        <p>10</p>
+      </li>
+
+      <li>
+        <p>Weeks since last report</p>
+        <p>2</p>
+      </li>
+
+      <li>
+        <p>Participation rate</p>
+        <p>50%</p>
+      </li>
+    </ul>
+  </aside>
+
+  <aside class="health-reports">
+    <h3 class="title-primary">Health Reports</h3>
+
+    <ul>
+      <li></li>
+    </ul>
+  </aside>
+</section>
 
 <?php include 'php/includes/footer.inc.php'; ?>
