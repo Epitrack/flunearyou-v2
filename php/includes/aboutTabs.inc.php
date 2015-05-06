42acea57c1137a366708dfe83480b66eea0cbc2b
Thulio Philipe <thulioph@gmail.com>
2015-05-06 16:18:08 -0300

    Adds new code to tabs.

--- php/includes/aboutTabs.inc.php
+++ php/includes/aboutTabs.inc.php
@@ -1,100 +1,114 @@
-<aside class="about-tabs">
-  <a href="#how-it-works" title="How it works">How it works</a>
-  <a href="#who-we-are" title="Who we are">Who we are</a>
+<aside role="tabpanel" class="about-tabs">
 
-  <aside id="how-it-works" class="tab">
-    <p class="description">
-      Thousands of individuals submit weekly health reports to benefit their
-      community. It’s the power of the crowd!
-    </p>
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active">
+      <a href="#how-it-works" title="How it works" aria-controls="how-it-works" role="tab" data-toggle="tab">How it works</a>
+    </li>
 
-    <div class="complementary">
-      <h4>Did you know?</h4>
-      <ul>
-        <li>
-          CDC estimates of flu-associated yearly deaths in the United States
-          range from a low of about 3,000 to a high of about 49,000 people.
-        </li>
+    <li role="presentation">
+      <a href="#who-we-are" title="Who we are" aria-controls="who-we-are" role="tab" data-toggle="tab">Who we are</a>
+    </li>
+  </ul>
 
-        <li>
-          If you have the flu, you can infect others 1 day before showing
-          symptoms and up to 7 days after becoming sick.
-        </li>
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <aside id="how-it-works" role="tabpanel" class="tab-pane active">
+      <p class="description">
+        Thousands of individuals submit weekly health reports to benefit their
+        community. It’s the power of the crowd!
+      </p>
 
-        <li>
-          The single best way to prevent the flu is to get a flu vaccine
-          each season.
-        </li>
-      </ul>
-    </div>
+      <div class="complementary">
+        <h4>Did you know?</h4>
+        <ul>
+          <li>
+            CDC estimates of flu-associated yearly deaths in the United States
+            range from a low of about 3,000 to a high of about 49,000 people.
+          </li>
 
-    <div class="complementary">
-      <aside>
-        <figure>
-          <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
-          <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
-        </figure>
-      </aside>
+          <li>
+            If you have the flu, you can infect others 1 day before showing
+            symptoms and up to 7 days after becoming sick.
+          </li>
 
-      <aside>
-        <figure>
-          <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
-          <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
-        </figure>
-      </aside>
+          <li>
+            The single best way to prevent the flu is to get a flu vaccine
+            each season.
+          </li>
+        </ul>
+      </div>
 
-      <aside>
-        <figure>
-          <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
-          <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
-        </figure>
-      </aside>
-    </div>
-  </aside>
+      <div class="complementary">
+        <aside>
+          <figure>
+            <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
+            <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
+          </figure>
+        </aside>
 
-  <aside id="who-we-are" class="tab">
-    <h4>Early detection and early response are key to preventing the spread of any disease.</h4>
-    <h5>Makes sense, but how do you detect disease outbreaks in real time?</h5>
+        <aside>
+          <figure>
+            <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
+            <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
+          </figure>
+        </aside>
 
-    <div class="complementary">
+        <aside>
+          <figure>
+            <img src="http://dummyimage.com/162x287/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image">
+            <figcaption>Take just a few seconds to report how you’ve been feeling. It’s free and anonymous</figcaption>
+          </figure>
+        </aside>
+      </div>
+    </aside>
+
+    <aside id="who-we-are" role="tabpanel" class="tab-pane active">
+      <h4>Early detection and early response are key to preventing the spread of any disease.</h4>
+      <h5>Makes sense, but how do you detect disease outbreaks in real time?</h5>
+
+      <div class="complementary">
+        <p>
+          While important systems exist for reporting and tracking disease,
+          they’re not perfect, missing those who don’t go to the doctor, and a
+          bit slowly because it takes time for reports to be collected and sent
+          to health departments.
+        </p>
+
+        <p>
+          We believe that letting individuals report symptoms in real-time can
+          complement traditional tracking while providing useful information
+          directly to the public.
+        </p>
+      </div>
+
+      <div class="complementary">
+        <figure><img src="" alt="" title=""></figure>
+        <figure><img src="" alt="" title=""></figure>
+        <figure><img src="" alt="" title=""></figure>
+      </div>
+
       <p>
-        While important systems exist for reporting and tracking disease,
-        they’re not perfect, missing those who don’t go to the doctor, and a
-        bit slowly because it takes time for reports to be collected and sent
-        to health departments.
+        Created by epidemiologists at Harvard, Boston Children’s Hospital and
+        The Skoll Global Threats Fund, Flu Near You relies on voluntary
+        participation from the general public, asking you to take a few
+        seconds each week to report if you or your family members have been
+        healthy or sick.
       </p>
 
       <p>
-        We believe that letting individuals report symptoms in real-time can
-        complement traditional tracking while providing useful information
-        directly to the public.
+        We analyze thousands of reports and map them to generate local and
+        national views of influenza-like illness, providing public health
+        officials and researchers with real-time, anonymous information that
+        could help prevent the next pandemic.
       </p>
-    </div>
 
-    <div class="complementary">
-      <figure><img src="" alt="" title=""></figure>
-      <figure><img src="" alt="" title=""></figure>
-      <figure><img src="" alt="" title=""></figure>
-    </div>
+      <p>
+        With your help, we can all see what’s coming and - better still- you
+        have the knowledge to protect yourself and your family against disease.
+      </p>
+    </aside>
+  </div>
 
-    <p>
-      Created by epidemiologists at Harvard, Boston Children’s Hospital and
-      The Skoll Global Threats Fund, Flu Near You relies on voluntary
-      participation from the general public, asking you to take a few
-      seconds each week to report if you or your family members have been
-      healthy or sick.
-    </p>
-
-    <p>
-      We analyze thousands of reports and map them to generate local and
-      national views of influenza-like illness, providing public health
-      officials and researchers with real-time, anonymous information that
-      could help prevent the next pandemic.
-    </p>
-
-    <p>
-      With your help, we can all see what’s coming and - better still- you
-      have the knowledge to protect yourself and your family against disease.
-    </p>
-  </aside>
 </aside>
+
