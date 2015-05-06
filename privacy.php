1bdf8793769d3cd752a4bddcd0f81c32523f9841
Thulio Philipe <thulioph@gmail.com>
2015-05-06 16:11:28 -0300

    Adds html to privacy page.

--- privacy.php
+++ privacy.php
@@ -1,5 +1,51 @@
 <?php include 'php/includes/head.inc.php'; ?>
 <?php include 'php/includes/navbar.inc.php' ?>
 
+<section class="wrapper-section">
+  <h2 class="title-section">Privacy Policy</h2>
+
+  <div class="content">
+    <p class="description">
+      We recognize the sensitive nature of Flu Near You questions, and take the
+      security and confidentiality of your information seriously. As we describe
+       below, your participation in this project will remain confidential.
+       Submissions will be anonymized and posted in aggregate with other
+       submissions. IP addresses are routinely recorded but will also remain
+       confidential.
+
+       <br><br>
+
+       Your contact information will not be shared with any third party except
+       as required by law.
+
+       <br><br>
+
+       We collect personal information such as, but not limited to, your email
+       address, age, gender and Internet Protocol address. We reserve the right
+       to use this information internally, including but not limited to
+
+       <br><br>
+
+       research purposes, quality assurance purposes, and correspondence with
+       you, but we will not sell, share, rent, or otherwise reveal this
+       information to any third party except as required by law, or to address
+       issues of noncompliance. We may share aggregate data with third parties
+       in a manner that will not identify you.
+
+       <br><br>
+
+       All data exchanged with this site is protected by SSL encryption, the
+       same standard used to protect the transmission of credit card
+       information and other sensitive data over the internet.
+
+       <br><br>
+
+       Flu Near You uses cookies to improve the quality of our service,
+       including (if you choose) remembering your login information so that it
+       does not need to be re-entered.
+    </p>
+  </div>
+</section>
+
 
 <?php include 'php/includes/footer.inc.php'; ?>
