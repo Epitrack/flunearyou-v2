d74b8aa3d3e34d21ca139f5210b9eb37e3714dbe
thulioph <thulioph@gmail.com>
2015-06-03 16:36:08 -0300

    Removes some scripts.

--- src/js/fny/APP.Vaccine_finder.js
+++
@@ -1,5 +0,0 @@
-$('#vaccine-finder-btn').click(function () {
-  // Add Google Analytics Event Tracking:
-  window.open("http://flushot.healthmap.org/?address=" + $('#vaccine-finder-input').val());
-  return false;
-});
