d377755e25db8b083415c58f82541e05058d497f
thulioph <thulioph@gmail.com>
2015-06-13 03:50:17 -0300

    Adds new modules and controllers.

--- src/js/angular/APP.Module.js
+++ src/js/angular/APP.Module.js
@@ -2,22 +2,40 @@
   'use strict';
 
   angular
-    .module('FnyApp', ['ngRoute', 'pressController', 'fluNewsController', 'faqController'])
+    .module('FnyApp', ['ngRoute', 'aboutController', 'fluNewsController', 'pressController', 'faqController', 'termsController', 'privacyController', 'homeController'])
     .config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
      $routeProvider
-      .when('/press', {
-        templateUrl: 'templates/press.html',
-        controller: 'PressCtrl'
+      .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutCtrl'
       })
       .when('/flu-news', {
         templateUrl: 'templates/flu-news.html',
         controller: 'FluNewsCtrl'
-      }).when('/faq', {
+      })
+      .when('/press', {
+        templateUrl: 'templates/press.html',
+        controller: 'PressCtrl'
+      })
+      .when('/faq', {
         templateUrl: 'templates/faq.html',
         controller: 'FaqCtrl'
-      }).when('/', {
-        templateUrl: 'templates/flu-news.html',
-        controller: 'FluNewsCtrl'
+      })
+      .when('/terms', {
+        templateUrl: 'templates/terms.html',
+        controller: 'TermsCtrl'
+      })
+      .when('/privacy', {
+        templateUrl: 'templates/privacy.html',
+        controller: 'PrivacyCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
       })
       .otherwise({
         redirectTo: '/'
