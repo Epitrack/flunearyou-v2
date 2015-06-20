fd093d2b3a23ca933a23fb51c58eaa21a6d8f292
thulioph <thulioph@gmail.com>
2015-06-20 19:05:32 -0300

    Adds new structure HTML, CSS and JS to flu-news page.

--- flu-news.php
+++ flu-news.php
@@ -6,29 +6,7 @@
 
   <div class="row">
 
-    <div class="col-xs-12 col-sm-4 col-md-3 ng-scope">
-      <article class="news-item">
-        <a href="http://www.usnews.com/news/articles/2015/06/17/after-ebola-is-the-us-ready-for-mers" title="After Ebola, Is the US Ready for MERS?" target="_blank" class="news-link-image">
-          <figure class="news-image">
-            <img src="http://img.scoop.it/k8sf9xRRtHsPjnQjGGCCKDl72eJkfbmt4t8yenImKBVVEcIaUH86nQbDznP1hDx3" alt="After Ebola, Is the US Ready for MERS?">
-          </figure>
-        </a>
-
-        <time datetime="05-May" class="news-date">
-          05 <span>May</span>
-        </time>
-
-        <div class="news-text">
-          <h3 class="news-title">
-            <a href="http://www.usnews.com/news/articles/2015/06/17/after-ebola-is-the-us-ready-for-mers" target="_blank" title="After Ebola, Is the US Ready for MERS?">After Ebola, Is the US Ready for MERS?</a>
-          </h3>
-
-          <p class="news-description"> The illness has already come to the U.S. twice, but was contained. Can it stay that way? </p>
-        </div>
-
-        <a href="http://www.usnews.com/news/articles/2015/06/17/after-ebola-is-the-us-ready-for-mers" title="Read More" target="_blank" class="read-more"> Read More </a>
-      </article>
-    </div>
+    <div id="flu-news-feeds"></div>
 
     <div class="col-xs-12">
       <a href="http://www.scoop.it" class="scoopit-logo" target="_blank" title="Scoop.it">
@@ -41,6 +19,34 @@
   </div>
 
 </section>
+
+<script id="flu-news-template" type="text/x-handlebars-template">
+  {{#each news}}
+    <div class="col-xs-12 col-sm-4 col-md-3">
+      <article class="news-item">
+        <a href="{{ url }}" title="{{ title }}" target="_blank" class="news-link-image">
+          <figure class="news-image">
+            <img src="{{ image }}" alt="{{ title }}">
+          </figure>
+        </a>
+
+        <time datetime="{{ day }} {{ month }}" class="news-date">
+          {{ day }} <span>{{ month }}</span>
+        </time>
+
+        <div class="news-text">
+          <h3 class="news-title">
+            <a href="{{ url }}" target="_blank" title="{{ title }}">{{ title }}</a>
+          </h3>
+
+          <p class="news-description"> {{ content }} </p>
+        </div>
+
+        <a href="{{ url }}" title="Read More" target="_blank" class="read-more"> Read More </a>
+      </article>
+    </div>
+  {{/each}}
+</script>
 
 <?php include 'php/includes/joinUs.inc.php'; ?>
 <?php include 'php/includes/footer.inc.php'; ?>
--- src/js/APP.FluNews.js
+++ src/js/APP.FluNews.js
@@ -1,14 +1,100 @@
 // ==========================================
-// Flu News
+// Flu News page
 // ==========================================
 
 var APP = APP || {};
 APP.FluNews = {
   setUp: function() {
-    this.request();
+    this.beforeRequest();
   },
 
-  request: function() {},
+  beforeRequest: function(){
+    var that, fluNewsStorage;
 
-  storage: function() {}
+    that = this;
+    fluNewsStorage = localStorage.getItem('flu-news-storage');
+
+    if (fluNewsStorage) {
+      that.templateOff(fluNewsStorage); // check if exist data
+    } else {
+      that.request(); // request data
+    }
+  },
+
+  request: function() {
+    var url, that, fluNewsFeeds, htmlElement, body;
+
+    that        = this;
+    url         = '/assets/flu-news.json';
+    htmlElement = document.querySelector('#flu-news-feeds');
+    body        = document.querySelector('body');
+
+    $.ajax({
+      url: url,
+      type: 'GET',
+
+      beforeSend: function() {
+        body.classList.add('js-loading');
+      },
+
+      success: function(data) {
+        body.classList.remove('js-loading');
+
+        fluNewsFeeds = data.data;
+
+        if (data.status === 200) {
+          that.template(fluNewsFeeds); // build template
+          that.insertIntoStorage(fluNewsFeeds); // insert into localstorage
+        } else {
+          htmlElement.innerHTML = fluNewsFeeds; // status !== 200
+        }
+      },
+
+      error: function(error) {
+        console.warn('Request error:', error);
+        htmlElement.innerHTML = error; // ajar error
+      }
+    });
+  },
+
+  templateOff: function(fluNewsStorage) {
+    var that;
+
+    that = this;
+
+    that.template(JSON.parse(fluNewsStorage)); // transform to JSON again
+  },
+
+  template: function(fluNewsFeeds) {
+    var source, template, output, fluNewsList, title, url, day, month, image, content;
+
+    source     = document.getElementById('flu-news-template').innerHTML;
+    template   = Handlebars.compile(source);
+    fluNewsList  = [];
+
+    for (var i = 0; i < fluNewsFeeds.length; i++) {
+      title   = fluNewsFeeds[i].title;
+      url     = fluNewsFeeds[i].link;
+      day     = fluNewsFeeds[i].day;
+      month   = fluNewsFeeds[i].month;
+      image   = fluNewsFeeds[i].image;
+      content = fluNewsFeeds[i].description;
+
+      fluNewsList.push({
+        title: title,
+        url: url,
+        day: day,
+        month: month,
+        image: image,
+        content: content
+      });
+
+      output = template({news: fluNewsList});
+      document.getElementById('flu-news-feeds').innerHTML = output;
+    }
+  },
+
+  insertIntoStorage: function(fluNewsFeeds) {
+    localStorage.setItem('flu-news-storage', JSON.stringify(fluNewsFeeds));
+  }
 }
