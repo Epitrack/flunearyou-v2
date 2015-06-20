8f8bb7e91a9f04bbd0328af4088cd4b798058cd3
thulioph <thulioph@gmail.com>
2015-06-20 18:27:06 -0300

    Adds new structure HTML, CSS and JS to press page.

--- press.php
+++ press.php
@@ -10,27 +10,33 @@
     <aside class="col-xs-12 col-sm-4 col-md-6 press-year">
       <h3 class="year">2015</h3>
 
-      <ul class="press-list">
-        <li class="press-item">
-          <a class="press-link" href="#" title="Título Aqui" target="_blank">
-            <time datetime="24-02">24 feb</time>
-            <div class="press-info">
-              <h4 class="press-title">Track the Flu With the 'Flu Near You</h4>
-              <p class="press-source">coldflu.about.com</p>
-            </div>
-          </a>
-
-          <aside class="press-share">
-            <a href="http://www.google.com.br" title="Share with Facebook" class="facebook">Facebook</a>
-            <a href="http://www.google.com.br" title="Share with Twitter" class="twitter">Twitter</a>
-          </aside>
-        </li>
-      </ul>
+      <ul class="press-list" id="press-list"></ul>
     </aside>
 
   </div>
 
 </section>
+
+<script id="press-template" type="text/x-handlebars-template">
+  <ul>
+    {{#each press}}
+      <li class="press-item">
+        <a class="press-link" href="{{ link }}" title="{{ title }}" target="_blank">
+          <time datetime="{{ time }}">{{ date }}</time>
+          <div class="press-info">
+            <h4 class="press-title">{{ title }}</h4>
+            <p class="press-source">{{ site }}</p>
+          </div>
+        </a>
+
+        <aside class="press-share">
+          <a href="{{ link }}" title="Share with Facebook" class="facebook">Facebook</a>
+          <a href="{{ link }}" title="Share with Twitter" class="twitter">Twitter</a>
+        </aside>
+      </li>
+    {{/each}}
+  </ul>
+</script>
 
 <?php include 'php/includes/joinUs.inc.php' ?>
 <?php include 'php/includes/footer.inc.php'; ?>
