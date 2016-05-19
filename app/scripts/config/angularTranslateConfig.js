125635c4f3b61065ff48316df00b110d0a552752
sfilhu <sfilhu@gmail.com>
2016-05-19 13:49:29 -0300

    ajuste translate

--- app/scripts/config/angularTranslateConfig.js
+++ app/scripts/config/angularTranslateConfig.js
@@ -8,6 +8,12 @@ app.config(['$translateProvider', function($translateProvider) {
 	$.get('http://dev.flunearyou.org/translations').success(function(data, status){
 		localStorage.setItem('translations_en', JSON.stringify(data.translations.en));
 		localStorage.setItem('translations_es', JSON.stringify(data.translations.es));
+		if(!localStorage.getItem('translations_en')){
+			alert('reload')
+			window.location.reload();
+		}else{
+			alert('no reload');
+		}
 	}).error(function(data, status){
 		console.log('Error in angularTranslateConfig.js');
 		console.log(data, status);
