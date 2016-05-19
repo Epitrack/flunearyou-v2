566f79ce1b9d0cfbdbe01f8351180cb6ac370218
Victor Luna <victorluna22@gmail.com>
2016-05-19 13:48:52 -0300

    get translations

--- app/scripts/config/angularTranslateConfig.js
+++ app/scripts/config/angularTranslateConfig.js
@@ -5,20 +5,37 @@
 
 app.config(['$translateProvider', function($translateProvider) {
 
-	$.get('http://dev.flunearyou.org/translations').success(function(data, status){
-		localStorage.setItem('translations_en', JSON.stringify(data.translations.en));
-		localStorage.setItem('translations_es', JSON.stringify(data.translations.es));
-	}).error(function(data, status){
-		console.log('Error in angularTranslateConfig.js');
-		console.log(data, status);
-	});
+	if (localStorage.getItem('translations_en') && localStorage.getItem('translations_es')){
+		
+		$translateProvider
+			.translations('en', JSON.parse(localStorage.getItem('translations_en')))
+			.translations('es', JSON.parse(localStorage.getItem('translations_es')))
+			.preferredLanguage('en')
+			.useSanitizeValueStrategy(null);
+
+	}else{
+		
+		$.get('http://dev.flunearyou.org/translations').success(function(data, status){
+			localStorage.setItem('translations_en', JSON.stringify(data.translations.en));
+			localStorage.setItem('translations_es', JSON.stringify(data.translations.es));
+			
+			$translateProvider
+				.translations('en', JSON.stringify(data.translations.en))
+				.translations('es', JSON.stringify(data.translations.es))
+				.preferredLanguage('en')
+				.useSanitizeValueStrategy(null);
+			$window.location.reload();
+
+		}).error(function(data, status){
+			console.log('Error in angularTranslateConfig.js');
+			console.log(data, status);
+		});
+	}
 
 
-	$translateProvider
-	.translations('en', JSON.parse(localStorage.getItem('translations_en')))
-	.translations('es', JSON.parse(localStorage.getItem('translations_es')))
-	.preferredLanguage('en')
-	.useSanitizeValueStrategy(null);
+
+
+	
 
 	
 }]);
