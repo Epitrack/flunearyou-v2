d4ac5b804f4442e1aa479e2547a0455eacd99ccb
sfilhu <sfilhu@gmail.com>
2016-05-30 17:30:08 -0300

    Login by token - done

--- app/scripts/beforeExecute.js
+++ app/scripts/beforeExecute.js
@@ -1,9 +1,22 @@
-app.factory( 'session', ['$http', '$urlBase', '$routeParams', '$q', function GetSession($http, $urlBase, $routeParams, $q){
+app.factory( 'session', ['$http', '$urlBase', '$routeParams', '$q', '$rootScope', '$window',
+    function GetSession($http, $urlBase, $routeParams, $q, $rootScope, $window){
     var defer = $q.defer();
 
     var tokenTracings = function(token){
-        console.log('token', token);
-        // TO DO: implement function
+        $http.get($urlBase+'/user', {headers: {'token': token}}).success(function(data, status){
+            var nickname  = data.info.basic.nickname,
+                userToken = data.info.basic.token,
+                userEmail = data.info.basic.email,
+                userLoggedObj = {
+                    'name'  : nickname,
+                    'email' : userEmail,
+                    'token' : userToken
+                };
+                
+                localStorage.setItem('userLogged', JSON.stringify(userLoggedObj));
+                $rootScope.$emit("IS_LOGGED");
+                $window.location.href = '#/report?token='+userToken;
+        }).error(function(data, status){ console.log(status) });
         return true;
     }
 
