14cdde370ab37647ce3c20d4f893b27f51f03ad0
sfilhu <sfilhu@gmail.com>
2016-09-22 14:56:38 -0300

    Change FBid

--- app/scripts/config/facebookConfig.js
+++ app/scripts/config/facebookConfig.js
@@ -10,8 +10,14 @@ app.config(['FacebookProvider',function(FacebookProvider) {
 	if (url.indexOf('localhost') != -1){
 		var FBid = '362068090500998';
 	}else{
+		// DEV
 		// var FBid = '463215990541721';
-		var FBid = '362068090500998';
+
+		// PROD
+		var FBid = '199700630106025';
+
+		// Localhost
+		// var FBid = '362068090500998';
 	}
 	FacebookProvider.init(FBid);
 }]); 
