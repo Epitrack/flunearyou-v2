eae278fc19914a04cf0c9bf28efe397e4977b3ea
sfilhu <sfilhu@gmail.com>
2016-10-01 13:22:49 -0300

    Remove comments

--- app/scripts/config/angularTranslateConfig.js
+++ app/scripts/config/angularTranslateConfig.js
@@ -42,36 +42,4 @@ app.config(['$translateProvider', function($translateProvider) {
 			console.log(status);
 		});
 	});
-
-
-
-	// if (localStorage.getItem('translations_en') && localStorage.getItem('translations_es')){
-		
-		$translateProvider
-			.translations('en', JSON.parse(localStorage.getItem('translations_en')))
-			.translations('es', JSON.parse(localStorage.getItem('translations_es')))
-			.preferredLanguage(language)
-			.useSanitizeValueStrategy(null);
-
-	// }else{
-		
-	// 	$.get('http://dev.flunearyou.org/translations').success(function(data, status){
-	// 		localStorage.setItem('translations_en', JSON.stringify(data.translations.en));
-	// 		localStorage.setItem('translations_es', JSON.stringify(data.translations.es));
-			
-	// 		$translateProvider
-	// 			.translations('en', JSON.stringify(data.translations.en))
-	// 			.translations('es', JSON.stringify(data.translations.es))
-	// 			.preferredLanguage(language)
-	// 			.useSanitizeValueStrategy(null);
-			
-	// 		window.location.reload();
-
-	// 	}).error(function(data, status){
-	// 		console.log('Error in angularTranslateConfig.js');
-	// 		console.log(data, status);
-	// 	});
-	// }
-
-	
 }]);
