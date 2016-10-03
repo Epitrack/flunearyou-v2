9e6e5d7be07a79902b6cb7192e6d771a2493c656
sfilhu <sfilhu@gmail.com>
2016-10-02 22:16:03 -0300

    return localStorage translate

--- app/scripts/config/angularTranslateConfig.js
+++ app/scripts/config/angularTranslateConfig.js
@@ -11,35 +11,33 @@ app.config(['$translateProvider', function($translateProvider) {
 
 	var language = localStorage.getItem('lng');
 
-	// Remove DB
-	// fnyDB.get('translations').then(function(data) {
-	// 	fnyDB.remove(data);
-	// });
-
-	
-	console.log('fnyDB.get(translate)');
-	fnyDB.get('translate').then(function(data){
+	if (localStorage.getItem('translations_en') && localStorage.getItem('translations_es')){
+		
 		$translateProvider
-			.translations('en', data.en)
-			.translations('es', data.es)
+			.translations('en', JSON.parse(localStorage.getItem('translations_en')))
+			.translations('es', JSON.parse(localStorage.getItem('translations_es')))
 			.preferredLanguage(language)
 			.useSanitizeValueStrategy(null);
-	}).catch(function(err){
+
+	}else{
+		
 		$.get('http://dev.flunearyou.org/translations').success(function(data, status){
-			var lngEN = JSON.stringify(data.translations.en),
-				lngES = JSON.stringify(data.translations.es);
-
-			fnyDB.put({
-				_id : 'translate',
-				en  : lngEN,
-				es  : lngES
-			});
-
+			localStorage.setItem('translations_en', JSON.stringify(data.translations.en));
+			localStorage.setItem('translations_es', JSON.stringify(data.translations.es));
+			
+			$translateProvider
+				.translations('en', JSON.stringify(data.translations.en))
+				.translations('es', JSON.stringify(data.translations.es))
+				.preferredLanguage(language)
+				.useSanitizeValueStrategy(null);
+			
 			window.location.reload();
+
 		}).error(function(data, status){
 			console.log('Error in angularTranslateConfig.js');
-			console.log(data);
-			console.log(status);
+			console.log(data, status);
 		});
-	});
+	}
+
+	
 }]);
